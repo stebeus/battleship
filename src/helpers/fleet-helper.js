cbbcd341957a60a809996bc0287c0fa4194e246b
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-22 15:50:40 -0300

    feat: create fleet factory

--- src/helpers/fleet-helper.js
+++ src/helpers/fleet-helper.js
@@ -11,4 +11,17 @@ function createDivision(quantity = 1, shipLength = 1) {
   return division;
 }
 
-export { createDivision };
+function createFleet(quantity) {
+  const fleet = [];
+
+  let shipLength = 1;
+
+  for (let division = 0; division < quantity; division++) {
+    const division = createDivision(quantity--, shipLength++);
+    fleet.push(division);
+  }
+
+  return fleet.flat();
+}
+
+export { createDivision, createFleet };

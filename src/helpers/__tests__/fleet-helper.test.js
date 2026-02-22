771e3b92608dcdcf90a56130dbca423093b4b58a
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-22 15:07:15 -0300

    test: add suite for default division length

--- src/helpers/__tests__/fleet-helper.test.js
+++ src/helpers/__tests__/fleet-helper.test.js
@@ -1,5 +1,9 @@
 import { describe, expect, it } from "vitest";
 
 describe("createDivision", () => {
-  describe("Default behavior", () => {});
+  describe("Default behavior", () => {
+    it("creates a division of 1 ship", () => {
+      expect(division).toHaveLength(1);
+    });
+  });
 });

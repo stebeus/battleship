c026ad3cf40ba696fb6d8fa9a01c845f5faea76f
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-22 14:11:29 -0300

    test: set up ship object

--- src/core/__tests__/ship.test.js
+++ src/core/__tests__/ship.test.js
@@ -1,4 +1,11 @@
-import { expect, it } from "vitest";
+import { beforeEach, expect, it } from "vitest";
+import { Ship } from "../ship";
+
+let ship;
+
+beforeEach(() => {
+  ship = new Ship();
+});
 
 it("creates a ship of length 1 by default", () => {
   expect(ship).toHaveLength(1);

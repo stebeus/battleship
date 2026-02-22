2b93e7a4158ae90c13e193b6a93161fab3de7a65
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-22 15:00:07 -0300

    feat: add columns to the grid array

--- src/helpers/grid-helper.js
+++ src/helpers/grid-helper.js
@@ -1,8 +1,12 @@
-function createGrid(rows) {
+function createGrid(rows, columns) {
   const grid = [];
+  const cell = 0;
 
   for (let row = 0; row < rows; row++) {
     grid[row] = [];
+    for (let column = 0; column < columns; column++) {
+      grid[row][column] = cell;
+    }
   }
 
   return grid;

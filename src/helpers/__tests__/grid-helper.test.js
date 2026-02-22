bb2434c24dc39b89effbe3d8d20bc2bce3b1f67e
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-22 14:51:56 -0300

    test: add unit for creation of a nine column grid

--- src/helpers/__tests__/grid-helper.test.js
+++ src/helpers/__tests__/grid-helper.test.js
@@ -1,8 +1,12 @@
 import { expect, it } from "vitest";
 import { createGrid } from "../grid-helper";
 
-const grid = createGrid(10);
+const grid = createGrid(10, 9);
 
 it("creates a grid with 10 rows", () => {
   expect(grid).toHaveLength(10);
+});
+
+it("creates a grid with 9 columns", () => {
+  expect(grid[0]).toHaveLength(9);
 });

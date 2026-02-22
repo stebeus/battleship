d67412dd38e5fa570913f2bd74a9acf9878615ab
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-22 18:29:30 -0300

    feat: create placer method

--- src/core/game-board.js
+++ src/core/game-board.js
@@ -6,6 +6,15 @@ class GameBoard {
     this.fleet = createFleet(4);
     this.grid = createGrid(10, 10);
   }
+
+  place(shipIndex, row, column, axis) {
+    const ship = this.fleet[shipIndex];
+
+    for (let cell = 0; cell < ship.length; cell++) {
+      if (axis === "x") this.grid[row][column++] = ship;
+      if (axis === "y") this.grid[row++][column] = ship;
+    }
+  }
 }
 
 export { GameBoard };

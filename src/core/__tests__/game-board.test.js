5137283d562944fb2ea54be3feb9ce23c9d7fc2d
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-22 17:34:31 -0300

    test: set up gameBoard grid

--- src/core/__tests__/game-board.test.js
+++ src/core/__tests__/game-board.test.js
@@ -1,8 +1,13 @@
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
+import { createGrid } from "../../helpers/grid-helper";
 import { GameBoard } from "../game-board";
 import { Ship } from "../ship";
 
 const gameBoard = new GameBoard();
 const ship = new Ship(2);
 
-describe("GameBoard.place", () => {});
+describe("GameBoard.place", () => {
+  beforeEach(() => {
+    gameBoard.grid = createGrid(3, 3);
+  });
+});

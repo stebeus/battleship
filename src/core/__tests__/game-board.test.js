21179aefec89eb8bb33180d4ee815d7f50c6267c
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-22 17:05:10 -0300

    test: add suite for GameBoard.place

--- src/core/__tests__/game-board.test.js
+++ src/core/__tests__/game-board.test.js
@@ -1,6 +1,8 @@
-import { expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import { GameBoard } from "../game-board";
 import { Ship } from "../ship";
 
 const gameBoard = new GameBoard();
 const ship = new Ship(2);
+
+describe("GameBoard.place", () => {});

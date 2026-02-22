aa15b35b87c5bc528d0909dc1ea12e2af65397ac
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-22 14:48:26 -0300

    test: set up grid array

--- src/helpers/__tests__/grid-helper.test.js
+++ src/helpers/__tests__/grid-helper.test.js
@@ -1,4 +1,7 @@
 import { expect, it } from "vitest";
+import { createGrid } from "../grid-helper";
+
+const grid = createGrid(10);
 
 it("creates a grid with 10 rows", () => {
   expect(grid).toHaveLength(10);

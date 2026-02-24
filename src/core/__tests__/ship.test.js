075772e5270bd1c7a6c6b41c14b8dc02fd0c5f46
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-24 06:54:16 -0300

    test: improve the sentence flow by adding "the"

--- src/core/__tests__/ship.test.js
+++ src/core/__tests__/ship.test.js
@@ -24,7 +24,7 @@ describe("Ship.isSunk", () => {
 });
 
 describe("Ship.hit", () => {
-  it("reduces ship health by 1 hit", () => {
+  it("reduces the ship health by 1 hit", () => {
     // Arrange
     const ship = new Ship(3);
 
@@ -35,7 +35,7 @@ describe("Ship.hit", () => {
     expect(ship.health).toBe(2);
   });
 
-  it("stops reducing ship health when it is 0", () => {
+  it("stops reducing the ship health when it is 0", () => {
     // Arrange
     const ship = new Ship(0);
 

e64cea7addf7e2efa3c6169e537eb019f114dec8
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-07 09:31:56 -0300

    feat: add board creator

--- src/components/board.js
+++ src/components/board.js
@@ -1,1 +1,15 @@
 import { parseHTML } from '../helpers/dom.js';
+import { createGrid } from './grid.js';
+
+function createBoard({ gameBoard: { grid }, name }) {
+  const gridContainer = createGrid(grid).outerHTML;
+
+  return parseHTML`
+    <div class="board" data-player="${name}">
+      <h2 class="board__player-name">${name}</h2>
+      ${gridContainer}
+    </div>
+  `;
+}
+
+export { createBoard };

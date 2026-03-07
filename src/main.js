98f07946124e571c1064145d741a0c048240905f
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-07 09:36:00 -0300

    feat: query select main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import './assets/style.css';
 import { Player } from './core/player.js';
 
+const main = document.querySelector('main');
+
 const human = new Player('Human');
 const robot = new Player('Robot');

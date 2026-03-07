01555eb046d1403ebd05d3f613fbe88b0bb39d97
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-07 09:55:32 -0300

    feat: add human and robot boards

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import './assets/style.css';
+import { createBoard } from './components/board.js';
 import { Player } from './core/player.js';
 
 const main = document.querySelector('main');
 
 const human = new Player('Human');
 const robot = new Player('Robot');
+
+const humanBoard = createBoard(human);
+const robotBoard = createBoard(robot);
+
+main.append(humanBoard, robotBoard);

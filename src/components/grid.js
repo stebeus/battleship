b6b085c85190faddd3621be13a832f3f3c683f93
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-07 09:25:59 -0300

    feat: add grid creator

--- src/components/grid.js
+++ src/components/grid.js
@@ -1,1 +1,19 @@
 import { parseHTML } from '../helpers/dom.js';
+
+function createGrid(grid) {
+  const container = parseHTML`<div class="grid"></div>`;
+
+  for (const row in grid) {
+    for (const column in grid[row]) {
+      const cell = parseHTML`            
+        <div class="grid__cell" data-coordinates="${row},${column}"></div>
+      `;
+
+      container.append(cell);
+    }
+  }
+
+  return container;
+}
+
+export { createGrid };

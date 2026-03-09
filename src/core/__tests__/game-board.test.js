96f81ca837f4bae88cd087d1a9ecdcd0b8edd880
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-09 13:57:28 -0300

    test: add unit for horizontal placement

--- src/core/__tests__/game-board.test.js
+++ src/core/__tests__/game-board.test.js
@@ -1,4 +1,4 @@
-import { beforeEach, describe } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 import { GameBoard } from '../game-board.js';
 import { Ship } from '../ship.js';
 
@@ -15,5 +15,20 @@ describe('GameBoard.place', () => {
     });
 
     const ship = new Ship(2);
+
+    it('places ship horizontally', () => {
+      // Arrange
+      const horizontalPlacement = [
+        [0, 0, 0],
+        [0, ship, ship],
+        [0, 0, 0],
+      ];
+
+      // Act
+      gameBoard.place(4, 1, 1, 'x');
+
+      // Assert
+      expect(gameBoard.grid).toStrictEqual(horizontalPlacement);
+    });
   });
 });

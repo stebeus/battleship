68ea731c8bb7b80de2bb5aa45e53c076fb60c54d
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-09 16:36:14 -0300

    test: set up horizontal placement

--- src/core/__tests__/game-board.test.js
+++ src/core/__tests__/game-board.test.js
@@ -88,7 +88,16 @@ describe('GameBoard.place', () => {
 
     describe('When placing ships adjacently,', () => {
       describe('And when its on sides,', () => {
-        describe('And when the existing ship is horizontal', () => {});
+        describe('And when the existing ship is horizontal', () => {
+          const ship = new Ship(2);
+
+          const horizontalPlacement = [
+            [0, 0, 0, 0],
+            [0, ship, ship, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+          ];
+        });
       });
 
       describe('And when its on corners,', () => {

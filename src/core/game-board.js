8ea3fdca2d3aacbe90820fff646f42c361eb6d1a
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-09 14:03:45 -0300

    feat: add place method

--- src/core/game-board.js
+++ src/core/game-board.js
@@ -6,6 +6,17 @@ class GameBoard {
   grid = createMatrix(10, 10);
 
   #emptyCell = 0;
+
+  place(shipIndex, row, column, axis) {
+    const ship = this.fleet[shipIndex];
+
+    for (let cell = 0; cell < ship.length; cell++) {
+      this.grid[row][column] = ship;
+
+      if (axis === 'x') column++;
+      if (axis === 'y') row++;
+    }
+  }
 }
 
 export { GameBoard };

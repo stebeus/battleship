d8d78b6f2758bf5f9c7f3119b803ba904d6c9ec5
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-09 13:14:59 -0300

    test: add unit for string input

--- src/utils/__tests__/errors.test.js
+++ src/utils/__tests__/errors.test.js
@@ -1,3 +1,10 @@
-import { describe } from 'vitest';
+import { describe, expect, it } from 'vitest';
+import { checkIsPositiveInteger } from '../errors.js';
 
-describe('checkIsPositiveInteger', () => {});
+describe('checkIsPositiveInteger', () => {
+  it('throws error for strings', () => {
+    expect(() => checkIsPositiveInteger('string', 'Value')).toThrowError(
+      'Value (string) is not a positive integer',
+    );
+  });
+});

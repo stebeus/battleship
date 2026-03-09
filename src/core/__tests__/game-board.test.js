4ed010c3709c657bfbd71ca7af34d7789d874f78
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-09 13:56:34 -0300

    test: set up gameBoard grid

--- src/core/__tests__/game-board.test.js
+++ src/core/__tests__/game-board.test.js
@@ -1,8 +1,16 @@
-import { describe } from 'vitest';
+import { beforeEach, describe } from 'vitest';
 import { GameBoard } from '../game-board.js';
 
 const gameBoard = new GameBoard();
 
 describe('GameBoard.place', () => {
-  describe('Given valid placements,', () => {});
+  describe('Given valid placements,', () => {
+    beforeEach(() => {
+      gameBoard.grid = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+      ];
+    });
+  });
 });

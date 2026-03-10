1025117ee4d2197548085f69440a9c11a25038e5
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-10 08:50:12 -0300

    feat: add cell render

--- src/components/cell.js
+++ src/components/cell.js
@@ -1,3 +1,5 @@
+import { parseCoordinates } from '../helpers/coordinates.js';
+
 class CellStyle {
   static of(...styles) {
     const createCellStyle = ([cellType, classModifier]) =>
@@ -19,3 +21,20 @@ function modifyCellStyle(cell, coordinates, classModifier) {
     cell.classList.add(classModifier);
   }
 }
+
+function renderCell(cell, gameBoard) {
+  const coordinates = cell.dataset.coords;
+  const [row, column] = parseCoordinates(coordinates);
+
+  const styles = CellStyle.of(
+    [gameBoard.isCellShip(row, column), 'ship'],
+    [gameBoard.isCellOfType(row, column, 'miss'), 'miss'],
+    [gameBoard.isCellOfType(row, column, 'hit'), 'hit'],
+  );
+
+  for (const { cellType, classModifier } of styles) {
+    if (cellType) modifyCellStyle(cell, coordinates, classModifier);
+  }
+}
+
+export { renderCell };

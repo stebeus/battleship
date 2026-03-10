d1186917d4a78eef9fb4b0f00595e5503c643474
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-10 07:50:36 -0300

    feat: add HTML serializer

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,6 +1,9 @@
+const serializeHtml = (value) =>
+  value instanceof Element ? value.outerHTML : value;
+
 function parseHtml(strings, ...values) {
   const template = document.createElement('template');
-  const html = String.raw({ raw: strings }, ...values);
+  const html = String.raw({ raw: strings }, ...values.map(serializeHtml));
 
   template.innerHTML = html;
 

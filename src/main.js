302ddc44ef32a4ff75e18fb9601f9e9af5591237
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-10 08:13:41 -0300

    feat: add human board

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+import { createBoard } from './components/board.js';
+import { players } from './controllers/game-controller.js';
 import './style.css';
 
 const main = document.querySelector('main');
+
+const humanBoard = createBoard(players[0]);
+
+main.append(humanBoard);

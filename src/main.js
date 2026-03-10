f1c4db81b2668deefb3fcafb89dffca95d949d0d
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-10 08:23:53 -0300

    feat: add robot board

--- src/main.js
+++ src/main.js
@@ -5,5 +5,6 @@ import { players } from './controllers/game-controller.js';
 const main = document.querySelector('main');
 
 const humanBoard = createBoard(players[0]);
+const robotBoard = createBoard(players[1]);
 
-main.append(humanBoard);
+main.append(humanBoard, robotBoard);

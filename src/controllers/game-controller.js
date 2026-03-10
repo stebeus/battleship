e88fb5aa7889ab0f323cec7c0580d8fbec5ed21b
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-10 09:34:13 -0300

    feat: add attack register

--- src/controllers/game-controller.js
+++ src/controllers/game-controller.js
@@ -1,5 +1,15 @@
+import { renderCell } from '../components/cell.js';
 import { Player } from '../core/player.js';
+import { parseCoordinates } from '../helpers/coordinates.js';
 
 const players = Player.of('Human', 'Robot');
 
-export { players };
+function registerAttack(cell, { gameBoard }) {
+  const coordinates = cell.dataset.coords;
+  const [row, column] = parseCoordinates(coordinates);
+
+  gameBoard.receiveAttack(row, column);
+  renderCell(cell, gameBoard);
+}
+
+export { players, registerAttack };
